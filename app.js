c8aee3f28756fb7dad360384681f100b489d3350
Kenneth Carter <kxccarter@gmail.com>
2020-06-29 13:27:00 -0500

    adds deleteDiv function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $(document).ready(init);
 
-const counter = 0;
+let counter = 0;
 
 
 function init() {
     console.log(`jQ`);
     $('#js-generate-btn').on('click', newDiv);
+    $('.container').on('click', '.js-delete', deleteDiv);
 }
 
 
@@ -13,7 +14,15 @@ function newDiv() {
     $('.container').append(`
     <div class="js-div-initial">
     <p class="">${counter}</p>
+
+    <button class="js-make-yellow">Yellow</button>
+    <button class="js-delete">Delete</button>
     </div>
     
     `)
+    counter++;
+}
+
+function deleteDiv() {
+    $(this).parent().remove();
 }
