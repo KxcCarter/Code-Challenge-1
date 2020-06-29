ce335f1941ad15895f469420e8b67413cadcead1
Kenneth Carter <kxccarter@gmail.com>
2020-06-29 13:18:38 -0500

    adds logic to create new empty div

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-document.ready(init);
+$(document).ready(init);
+
+const counter = 0;
+
 
 function init() {
     console.log(`jQ`);
+    $('#js-generate-btn').on('click', newDiv);
+}
 
+
+function newDiv() {
+    $('.container').append(`
+    <div class="js-div-initial">
+    <p class="">${counter}</p>
+    </div>
+    
+    `)
 }
